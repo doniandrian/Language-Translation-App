d67736b37e076aea7f0d89ac27f857b6f1105725
faisaalsp <faisaalsp@gmail.com>
2024-10-09 22:17:56 +0700

    bikin navbar + footer

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./navbar/Footer";
+import Navbar from "./navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="w-screen"></div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
